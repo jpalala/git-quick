2597d5a91b9d8059c33e7413bd31353b60012b06
Joe Palala <jose.palala@live.com>
2020-01-11 22:50:12 +0800

    added git-guilt

--- showhist.js
+++ showhist.js
@@ -1,11 +1,31 @@
 var shell = require('shelljs');
+var gg = require('git-guilt');
+const program = require('commander');
+
+program
+  .option('-g, --guilt', 'output guilt 1 guilt log down')
+  .option('-log, --log', 'output the log');
+
+program.parse(process.argv);
 
 if (!shell.which('git')) {
   shell.echo('Sorry, this script requires git');
   shell.exit(1);
 }
 
-if (shell.exec('git log --pretty=format:"%h - %an, %ar : %s"').code !== 0) {
-  shell.echo('Error: Git commit failed');
-  shell.exit(1);
+console.log(program.opts());
+
+if(program.guilt) {
+	shell.exec('git-guilt HEAD~1 HEAD');
 }
+
+if(program.log) {
+	if (shell.exec('git log --pretty=format:"%h - %an, %ar : %s"').code !== 0) {
+	  shell.echo('Error: Git commit failed');
+	  shell.exit(1);
+	}
+}
+
+
+
+
