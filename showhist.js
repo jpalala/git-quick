c6d04e26564c5f59b95e14748fefdb045ddabf82
Joe Palala <jose.palala@live.com>
2020-01-11 22:28:58 +0800

    initial commit

---
+++ showhist.js
@@ -0,0 +1,11 @@
+var shell = require('shelljs');
+
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git');
+  shell.exit(1);
+}
+
+if (shell.exec('git log --pretty=format:"%h - %an, %ar : %s"').code !== 0) {
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}
